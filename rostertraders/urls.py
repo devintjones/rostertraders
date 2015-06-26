24e995ced8677296e46f1e5f5df318b9edc7165b
devintjones <devin.thatcher.jones@gmail.com>
2015-06-26 01:42:49 -0400

    added static files, apps for home and dashboard, templates

--- rostertraders/urls.py
+++ rostertraders/urls.py
@@ -1,10 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns,include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rostertraders.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+	#url(r'^login/$', 'django.contrib.auth.views.login'),
+	#url(r'^logout/$', 'django.contrib.auth.views.logout'),
+	url(r'^players','app1.views.player_vals'),
+	url(r'^$', 'apps.home.views.index'),
+	#url('', include('social.apps.django_app.urls', namespace='social')),
+	url(r'^contact/$','app1.views.contact'),
+	url(r'^logout/$', 'app1.views.logout'),
+	url(r'^sell_shares/$', 'app1.views.sell_shares'),
+	url(r'^buy_shares/$', 'app1.views.buy_shares'),
+	url(r'^admin/', include(admin.site.urls)),
+]
